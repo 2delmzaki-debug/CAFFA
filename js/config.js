31ab609f710370454249505671491241fe27e9e1
2delmzaki-debug <2del.mzaki@gmail.com>
2026-05-23 19:49:28 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// ================================================
+// إعدادات Supabase — مطعم النخبة
+// ================================================
+
+const SB_URL = 'https://bfqvzsgyhygvtivqyrba.supabase.co';
+const SB_KEY = 'sb_publishable__7XaScQq44EhCvXix1f6zg_-';

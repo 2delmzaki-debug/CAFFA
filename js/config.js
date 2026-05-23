4a09581fb4f4d7a1661a0bfcd1f561c150f60356
2delmzaki-debug <2del.mzaki@gmail.com>
2026-05-23 18:30:13 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,8 +4,8 @@
 // 1. اذهب إلى supabase.com → مشروعك → Settings → API
 // 2. انسخ Project URL و anon/public key
 
-sb_publishable_ypWd3Pn682MGJQGPBCbuig_y8AGQxC8
-sb_secret_VfLhXjnjy0VXAyQo01Oebg_xYNID9Dn
+const SB_URL = sb_publishable_ypWd3Pn682MGJQGPBCbuig_y8AGQxC8
+const SB_URL = sb_secret_VfLhXjnjy0VXAyQo01Oebg_xYNID9Dn
 
 // مثال:
 // const SB_URL = 'https://abcdefgh.supabase.co';

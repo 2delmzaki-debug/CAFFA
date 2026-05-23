9962fde6b183e9e9479e9a39be24d4c27a1c9d82
2delmzaki-debug <2del.mzaki@gmail.com>
2026-05-23 18:53:20 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,8 +5,8 @@
 // 2. انسخ Project URL و anon/public key
 
 // ⚙️ إعدادات Supabase - مطعم النخبة
-window.SUPABASE_URL = 'sb_publishable_ypWd3Pn682MGJQGPBCbuig_y8AGQxC8';
-window.SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5aWZ3eGdhc2ZxeGZja2p4Z2luIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1Mzc5NjgsImV4cCI6MjA5NTExMzk2OH0.BrLQfiMdnRwD5menkU0K-2H5Ywl12cq1JcXtC36DYnU';
+
+sb_publishable_ypWd3Pn682MGJQGPBCbuig_y8AGQxC8
 
 // مثال:
 // const SB_URL = 'https://abcdefgh.supabase.co';

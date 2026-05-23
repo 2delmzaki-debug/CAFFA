e0d337a2ac8b9145d76a9e9f344fe8377d92aad6
2delmzaki-debug <2del.mzaki@gmail.com>
2026-05-23 19:58:42 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 // إعدادات Supabase — مطعم النخبة
 // ================================================
 
-const SB_URL = 'https://bfqvzsgyhygvtivqyrba.supabase.co';
-const SB_KEY = 'sb_publishable__7XaScQq44EhCvXix1f6zg_-';
+window.SUPABASE_URL = 'sb_publishable__7XaScQq44EhCvXix1f6zg_-a8gryq2';
+window.SUPABASE_KEY = 'bfqvzsgyhygvtivqyrba';
